72e3a6d5daab3fdf09a7932fd939df63db35f7b2
shiva <shiva@LAPTOP-8OIL1NPU>
2023-03-25 11:34:32 +0530

    Add project files.

---
+++ Appservice-MVC-SampleApp1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Appservice_MVC_SampleApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Appservice_MVC_SampleApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Appservice-MVC-SampleApp1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Appservice_MVC_SampleApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
